24a61b6c97131ccd1dafd813f39e6e1b632a5e2f
Roxy <155686159+RoxyKeatings@users.noreply.github.com>
2024-04-09 13:54:10 -0400

    modified more CSS & prompt details

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let apiKey = "89b05tfca20b16d5f5e3c646e1oa37db";
   let prompt = `User's instructions : Generate an eloquent poem about ${inputInstructionsElement.value}`;
   let context =
-    " You are a intellectual poet, please provide 4 lines of context, separate each line of the poem onto its own individual line using basic HTML format, write the poem based off the users instructions, write clearly and eloquently. End each poem on its own line with <strong> - AI Anonymous Poet Society";
+    " You are a intellectual poet, please provide 4 lines of context, always separate each line of the poem onto its own individual line using basic HTML format, write the poem based off the users instructions, write clearly and eloquently. End each poem on its own line with <strong> - AI Anonymous Poet Society";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   console.log("generating poem");
   console.log(`prompt${prompt}`);
