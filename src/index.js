ba34ac3ad5dfa77bf679f93470019ba2f09d9a7d
Roxy <155686159+RoxyKeatings@users.noreply.github.com>
2024-04-09 11:24:40 -0400

    added basic form handling & typewriter plug in

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: ["this is where the poems will be seen by users "],
+    autoStart: true,
+    pauseFor: 30000,
+    delay: 30,
+    cursor: "🖋️",
+  });
+}
+let poemFormElement = document.querySelector("#poem-topic-form");
+poemFormElement.addEventListener("submit", generatePoem);
