e2f202f46a0948e9055b28a8c0075076fb7e6a64
Roxy <155686159+RoxyKeatings@users.noreply.github.com>
2024-04-09 13:48:21 -0400

    altered API prompt

--- src/index.js
+++ src/index.js
@@ -12,11 +12,13 @@ function generatePoem(event) {
   event.preventDefault();
   let inputInstructionsElement = document.querySelector("#input-instructions");
   let apiKey = "89b05tfca20b16d5f5e3c646e1oa37db";
-  let prompt = `Generate an eloquent poem about ${inputInstructionsElement.value}`;
+  let prompt = `User's instructions : Generate an eloquent poem about ${inputInstructionsElement.value}`;
   let context =
-    " You are a intellectual poet, please provide 4 lines of context, separate each line of the poem onto its own individual line in basic HTML format, write the poem based off the users instructions, write clearly and eloquently, and end each poem with - AI Anonymous Poet Society";
+    " You are a intellectual poet, please provide 4 lines of context, separate each line of the poem onto its own individual line using basic HTML format, write the poem based off the users instructions, write clearly and eloquently. End each poem on its own line with <strong> - AI Anonymous Poet Society";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   console.log("generating poem");
+  console.log(`prompt${prompt}`);
+  console.log(`context${context}`);
   axios.get(apiUrl).then(displayPoem);
 }
 
